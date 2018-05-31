53562ba7664177e404ad49b595d4184b57415b75
ambuja <ambuja.magaji@yahoo.com>
2018-05-31 12:06:12 -0400

    contact form

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="from: $name \n Message: $message";
-$recipient = "ambuja.magaji@gmail.com";
+$recipient = "ambuja.magaji@yahoo.com";
 $subject ="Contact Form";
 $mailheader ="From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error");
