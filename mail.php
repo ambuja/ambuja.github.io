755ce6ecf19c3274043a26b831b6d090ad660bef
ambuja <ambuja.magaji@yahoo.com>
2018-05-31 16:01:26 -0400

    form

--- mail.php
+++ mail.php
@@ -1,4 +1,5 @@
 <?php
+/*
  $name = $_POST ['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -8,5 +9,5 @@ $subject ="Contact Form";
 $mailheader ="From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error");
 echo "Thank You!";
-?>
+?> */
 
